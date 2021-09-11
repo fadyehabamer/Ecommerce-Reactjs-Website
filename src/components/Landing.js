70be43d216756d1b0779f647de110c21854cf6bc
fadyehabamer <fadyamer45@gmail.com>
2021-09-11 18:28:16 +0200

    added stats sec in landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -15,8 +15,111 @@ export default class Landing extends Component {
                             <img className="banner" src="../img/landing.svg" alt="" />
                         </div>
                     </div>
-
                 </div>
+
+                <div className="container stats my-5">
+                    <div className="col-md-12">
+                    <h2 className="main-text">
+                        Our Statistics
+                    </h2>
+                        <div className="row ">
+                            <div className="col-xl-3 col-lg-6">
+                                <div className="card l-bg-cherry">
+                                    <div className="card-statistic-3 p-4">
+                                        <div className="card-icon card-icon-large"><i className="fas fa-shopping-cart"></i></div>
+                                        <div className="mb-4">
+                                            <h5 className="card-title mb-0">New Orders</h5>
+                                        </div>
+                                        <div className="row align-items-center mb-2 d-flex">
+                                            <div className="col-8">
+                                                <h2 className="d-flex align-items-center mb-0">
+                                                    3,243
+                                                </h2>
+                                            </div>
+                                            <div className="col-4 text-right">
+                                                <span  className="stat-percent">1% <i className="fa fa-arrow-up"></i></span>
+                                            </div>
+                                        </div>
+                                        <div className="progress mt-1 " data-height="8">
+                                            <div className="progress-bar l-bg-cherry" role="progressbar" data-width="25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" ></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-3 col-lg-6">
+                                <div className="card l-bg-blue-dark">
+                                    <div className="card-statistic-3 p-4">
+                                        <div className="card-icon card-icon-large"><i className="fas fa-users"></i></div>
+                                        <div className="mb-4">
+                                            <h5 className="card-title mb-0">Customers</h5>
+                                        </div>
+                                        <div className="row align-items-center mb-2 d-flex">
+                                            <div className="col-8">
+                                                <h2 className="d-flex align-items-center mb-0">
+                                                    15.07k
+                                                </h2>
+                                            </div>
+                                            <div className="col-4 text-right">
+                                                <span  className="stat-percent">9% <i className="fa fa-arrow-up"></i></span>
+                                            </div>
+                                        </div>
+                                        <div className="progress mt-1 " data-height="8">
+                                            <div className="progress-bar l-bg-blue-dark" role="progressbar" data-width="25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" ></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-3 col-lg-6">
+                                <div className="card l-bg-green-dark">
+                                    <div className="card-statistic-3 p-4">
+                                        <div className="card-icon card-icon-large"><i className="fas fa-ticket-alt"></i></div>
+                                        <div className="mb-4">
+                                            <h5 className="card-title mb-0">Items Buyed</h5>
+                                        </div>
+                                        <div className="row align-items-center mb-2 d-flex">
+                                            <div className="col-8">
+                                                <h2 className="d-flex align-items-center mb-0">
+                                                    578
+                                                </h2>
+                                            </div>
+                                            <div className="col-4 text-right">
+                                                <span className="stat-percent">5% <i className="fa fa-arrow-up"></i></span>
+                                            </div>
+                                        </div>
+                                        <div className="progress mt-1 " data-height="8">
+                                            <div className="progress-bar l-bg-green-dark" role="progressbar" data-width="25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-3 col-lg-6">
+                                <div className="card l-bg-orange-dark">
+                                    <div className="card-statistic-3 p-4">
+                                        <div className="card-icon card-icon-large"><i className="fas fa-dollar-sign"></i></div>
+                                        <div className="mb-4">
+                                            <h5 className="card-title mb-0">Revenue Today</h5>
+                                        </div>
+                                        <div className="row align-items-center mb-2 d-flex">
+                                            <div className="col-8">
+                                                <h2 className="d-flex align-items-center mb-0">
+                                                    $11.61k
+                                                </h2>
+                                            </div>
+                                            <div className="col-4 text-right">
+                                                <span className="stat-percent">2.5% <i className="fa fa-arrow-up"></i></span>
+                                            </div>
+                                        </div>
+                                        <div className="progress mt-1 " data-height="8" >
+                                            <div className="progress-bar l-bg-orange-dark" role="progressbar" data-width="25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" ></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+  
+ 
 
 
             </React.Fragment>
