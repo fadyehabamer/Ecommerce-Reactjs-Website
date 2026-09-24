f62572277104b85bfa2a992fd8d01b011e210dd7
fadyehabamer <fadyamer45@gmail.com>
2026-09-24 16:23:38 +0300

    fix(images): load illustrations from PUBLIC_URL instead of ../img
    The relative ../img/*.svg paths only resolve from top-level routes; on
    nested URLs (e.g. the 404 page for /foo/bar) they pointed at /foo/img/
    and the image broke.

--- src/components/Error.js
+++ src/components/Error.js
@@ -9,7 +9,7 @@ export default class Default extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-12">
-                            <img className="mx-auto d-block banner" alt="404" src="../img/404.svg" />
+                            <img className="mx-auto d-block banner" alt="404" src={`${process.env.PUBLIC_URL}/img/404.svg`} />
                             <h3 className="text-center" >
                                 The requested URL{"  "}
                                 <span className="text-danger
--- src/components/RedirectSignup.js
+++ src/components/RedirectSignup.js
@@ -7,7 +7,7 @@ export default function RedirectSignup() {
             <div className="container">
                 <div className="row">
                     <div className="col-12">
-                        <img className="mx-auto d-block banner" alt="redirect" src="../img/redirect.svg" />
+                        <img className="mx-auto d-block banner" alt="redirect" src={`${process.env.PUBLIC_URL}/img/redirect.svg`} />
                         <h1 className="mt-4 text-center" >
                             You must <span style={{borderBottom:"2px solid #ff9900"}}>Register</span> first<br/>
                         </h1>
